dadedb61ae077ac9abc97c864ea75bceb137bc12
Alexander Ivanchenko <alexsergivan@gmail.com>
2015-07-01 23:50:55 +0300

    Changed base styling

--- templates/mixitup_views_sorting.tpl.php
+++ templates/mixitup_views_sorting.tpl.php
@@ -4,7 +4,7 @@
  * Template for MixItUp sorting items.
  */
 ?>
-<div class="sort_wrapper">
+<div class="sort_wrapper form-item">
   <strong><?php print t('Sort by'); ?></strong>
   <div class="sort" data-sort="default"><?php print t('Default'); ?></div>
   <?php foreach ($sorts as $sort_field => $label): ?>
