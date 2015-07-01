ee7ccffe6f3fcf1ae6d86240e09272d3dd813fea
Alexander Ivanchenko <alexsergivan@gmail.com>
2015-07-01 21:54:44 +0300

    Added sorting functionality

--- views-view-mixitup.tpl.php
+++ views-view-mixitup.tpl.php
@@ -10,7 +10,9 @@
 <?php endif; ?>
 
 <?php foreach ($rows as $id => $row): ?>
-  <div class="mix_item mix<?php print ($classes_array[$id]) ? ' ' . $classes_array[$id] : ''; ?> <?php print ' ' . $view->result[$id]->classes; ?>">
-    <?php print $row; ?>
+  <div class="mix_item mix<?php print ($classes_array[$id]) ? ' ' . $classes_array[$id] : ''; ?>
+    <?php print ' ' . $view->result[$id]->classes; ?>"
+    <?php print isset($view->result[$id]->sorts) ? $view->result[$id]->sorts : ''; ?>>
+      <?php print $row; ?>
   </div>
 <?php endforeach; ?>
